edff46797a6a69e86ac53ede4ea9a55cbbfd3847
Ailin-Miscles-5J <a.22308051281264@cbtis128.edu.mx>
2024-09-09 10:13:06 -0600

    Add files via upload

---
+++ mi_lista.py
@@ -0,0 +1,17 @@
+
+print("Ejemplo de listas")
+arcoiris = ["Verde","Azul","Morado"]
+print (arcoiris)
+longitud=len(arcoiris)
+print("Elementos que contiene la lista arcoiris" , longitud)
+print(f"Elementos que contiene la lista arcoiris v2 {longitud}")
+print("accediendo a un elemento de la lista")
+print(arcoiris[1])
+print(f"elemento en la posiscion 0 es {arcoiris[0]}")
+print("agregar un elemento de la lista")
+print(arcoiris)
+arcoiris.append("Naranja")
+print(arcoiris)
+print("Listar los elementos de la lista cliclo for")
+for Ruvalcaba in arcoiris:
+    print(Ruvalcaba)
---
+++ mi_tuplas.py
@@ -0,0 +1,7 @@
+print("Ejemplo de tuplas")
+canciones=("Te amo","El NoNoa","Amor Eterno")
+print(canciones)
+y = list(canciones)
+y[1] = "La Puerta Negra"
+x = tuple(y)
+print(x)
