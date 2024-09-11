e6d31d3193eca1866a83e852044f46da48ec0e5d
Ailin-Miscles-5J <a.22308051281264@cbtis128.edu.mx>
2024-09-11 07:50:52 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,6 @@
+print("Ejemplo de conjuntos set")
+tipodeportes={"futbol","tenis","basquetbol"}
+print(tipodeportes)
+print("Lista elementos del conjunto tipodeportes loop for")
+for lin in tipodeportes:
+    print(lin)
---
+++ mi_diccionario.py
@@ -0,0 +1,7 @@
+lin = { "Gato": "blanco",
+       "perro": "chihuahua",
+       "año": 2007}
+print(lin)
+
+for Ruvalcaba in lin:
+    print(Ruvalcaba)
